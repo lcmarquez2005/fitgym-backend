9c6b6b5624c2459c8d72fad59a1f838eb9584871
lcmarquez2005 <lcarlosmarquezstrociak@gmail.com>
2026-04-15 08:49:03 -0600

    gitiignore added

---
+++ src/main/java/org/example/fitgymbackend/FitgymBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.fitgymbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitgymBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitgymBackendApplication.class, args);
+	}
+
+}
