c25082f14542c16f358def6b6532efb071e6485e
lcmarquez2005 <lcarlosmarquezstrociak@gmail.com>
2026-04-15 08:52:52 -0600

    gitiignore added env

--- src/main/java/org/example/fitgymbackend/FitgymBackendApplication.java
+++ src/main/java/org/example/fitgymbackend/FitgymBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FitgymBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FitgymBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FitgymBackendApplication.class, args);
+    }
 
 }
--- src/test/java/org/example/fitgymbackend/FitgymBackendApplicationTests.java
+++ src/test/java/org/example/fitgymbackend/FitgymBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FitgymBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
