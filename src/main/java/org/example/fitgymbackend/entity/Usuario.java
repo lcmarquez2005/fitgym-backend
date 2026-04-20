0fa73fd6a0476331c76f7d682b6d2e526f325e17
lcmarquez2005 <lcarlosmarquezstrociak@gmail.com>
2026-04-20 08:11:02 -0600

    .env.example agregar

--- src/main/java/org/example/fitgymbackend/entity/Usuario.java
+++ src/main/java/org/example/fitgymbackend/entity/Usuario.java
@@ -1,4 +1,17 @@
 package org.example.fitgymbackend.entity;
 
-public class Usuarios {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "usuarios")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String email;
 }
