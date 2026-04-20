2d7e80dfcbb524e69e5551f452a64680082ef22d
Rodrigo <luisrodrigo1005@gmail.com>
2026-04-20 08:03:53 -0600

    cambios para el .env

--- src/main/java/org/example/fitgymbackend/FitgymBackendApplication.java
+++ src/main/java/org/example/fitgymbackend/FitgymBackendApplication.java
@@ -7,7 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FitgymBackendApplication {
 
     public static void main(String[] args) {
+        // Cargar variables del .env
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
         SpringApplication.run(FitgymBackendApplication.class, args);
     }
-
 }
