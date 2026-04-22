4d946b74ba9173a9f0e06f70b00f4aa0396b6ffd
Rodrigo <luisrodrigo1005@gmail.com>
2026-04-22 03:46:04 -0600

    Actualizando master con AltaUsuarios rama

--- src/main/java/org/example/fitgymbackend/controller/UsuarioController.java
+++ src/main/java/org/example/fitgymbackend/controller/UsuarioController.java
@@ -8,15 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;  // 👈 IMPORT NUEVO
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;  // 👈 IMPORT NUEVO
-import java.util.Map;      // 👈 IMPORT NUEVO
+import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 import java.util.UUID;
 
